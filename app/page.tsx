8b85484c9b14103e70545346eaf46bfea139ba05
Kerry Sabanty <kgsabanty@gmail.com>
2023-10-02 19:28:19 -0400

    new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import NewTodoForm from '@/componentrs/NewTodoForm'
 
 export default function Home() {
   return (
     <div>
       <h1>home</h1>
+      <NewTodoForm></NewTodoForm>
     </div>
   )
 }
